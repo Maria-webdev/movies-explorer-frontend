bf8800cfe13ebda0f78f31cbef8ff6b450b8224f
Maria Averina <amaria.webdev@gmail.com>
2021-11-23 22:21:12 +0300

    add App, Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,28 @@
-// import React, { useState } from "react";
-// import { Route, Switch, Redirect, useHistory } from "react-router-dom";
-// import "../index.css";
-// import Header from "./Header";
-// import Main from "./Main";
-// import Footer from "./Footer";
-// import ImagePopup from "./ImagePopup";
-// import CurrentUserContext from "../contexts/CurrentUserContext";
-// import EditProfilePopup from "./EditProfilePopup";
-// import EditAvatarPopup from "./EditAvatarPopup";
-// import AddPlacePopup from "./AddPlacePopup";
-// import Login from "./Login";
-// import Register from "./Register";
-// import ProtectedRoute from "./ProtectedRoute";
-// import InfoTooltip from "./InfoTooltip";
-// import * as auth from "../utils/auth";
-// import api from "../utils/api";
+import React from 'react'; 
+import './App.css'; 
+import Header from './Header'; 
+import Main from './Main'; 
+import Footer from './Footer'; 
 
 
-function App() {
+return ( 
+
+ 
+
+  ( 
+
+    <> 
+
+      <div className='page__container'> 
+        <Header /> 
+        <Main /> 
+        <Footer /> 
+      </div> 
 
 
-  return (
 
-    <CurrentUserContext.Provider value={currentUser}>
-      <>
-        <div className="page__container">
-          <Header loggedIn={loggedIn} email={email} onSignout={handleLogout} />
-          <Switch>
-            <ProtectedRoute
-              exact
-              path="/"
-              component={Main}
-              onEditAvatar={handleEditAvatarClick}
-              onEditProfile={handleEditProfileClick}
-              onAddPlace={handleAddPlaceClick}
-              onCardClick={handleCardClick}
-              onCardLike={handleCardLike}
-              onCardDelete={handleCardDelete}
-              cards={cards}
-              loggedIn={loggedIn}
-            />
-            <Route path="/signin">
-              <Login onLogin={handleLogin} />
-            </Route>
-            <Route path="/signup">
-              <Register onRegister={handleRegister} />
-            </Route>
-            <Route> {loggedIn ? <Redirect to="/" /> : <Redirect to="/signin" />}</Route>
-          </Switch>
-          <Footer />
-        </div>
-        <EditProfilePopup isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} onUpdateUser={handleUpdateUser} />
-        <EditAvatarPopup isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} onUpdateAvatar={handleUpdateAvatar} />
-        <AddPlacePopup isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} onAddPlace={handleAddPlaceSubmit} />
-        <ImagePopup card={selectedCard !== null && selectedCard} onClose={closeAllPopups} />
-        <InfoTooltip isOpen={isInfoTooltipOpen} onClose={closeAllPopups} success={isSuccess} />
-      </>
-    </CurrentUserContext.Provider>
-  );
-}
+    </> 
 
-export default App;
+  ) 
+
+); 
---
+++ src/components/Main/Main.js
@@ -0,0 +1,34 @@
+import React from 'react'; 
+import './Main.css'; 
+import Promo from '../Promo/Promo'; 
+import NavTab  from '../NavTab/NavTab';
+import AboutProject  from '../AboutProject/AboutProject'; 
+import Techs from '../Techs/Techs'; 
+import AboutMe from '../AboutMe/AboutMe'; 
+import Portfolio  from '../Portfolio/Portfolio'; 
+
+
+return ( 
+
+ 
+
+  ( 
+
+    <> 
+
+      <div> 
+        <Promo /> 
+        <NavTab /> 
+        <AboutProject /> 
+        <Techs /> 
+        <AboutMe /> 
+        <Portfolio /> 
+      </div> 
+
+
+
+    </> 
+
+  ) 
+
+); 
