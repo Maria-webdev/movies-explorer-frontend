5fe409e41ef684c4a25d68e7c9e8ff3dfc4be345
Maria Averina <amaria.webdev@gmail.com>
2021-12-06 20:19:02 +0300

    fix Promo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,7 +7,7 @@ function Login() {
   return (
     <section className='login'>
       <div className='login__block'>
-        <Logo className='logo logo_login' />
+        <Logo className='logo center' />
         <h2 className='login__heading'>Рады видеть!</h2>
       </div>
 
