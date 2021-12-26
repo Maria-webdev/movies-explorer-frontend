9f18bb1fef1467a4aa048a1d89b588d7320df9b2
Maria Averina <amaria.webdev@gmail.com>
2021-12-26 14:04:12 +0300

    try to fix shotrMoviesButton

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import './SearchForm.css';
 import SearchIcon from '../../images/search_icon.svg';
 import useFormWithValidation from '../../hooks/useFormWithValidation';
@@ -6,8 +7,9 @@ import useFormWithValidation from '../../hooks/useFormWithValidation';
 function SearchForm(props) {
   const [isShortMovie, setIsShortMovie] = React.useState(false);
   const [isEmptyInput, setIsEmptyInput] = React.useState(true);
-
-
+  const { pathname } = useLocation();
+  const isMoviesPage = pathname === '/movies';
+  const isSavedMoviesPage = pathname === '/saved-movies';
 
   function handlePick() {
     props.isShortMovie(!isShortMovie);
@@ -25,6 +27,23 @@ function SearchForm(props) {
     }
   }
 
+  // кнопка Короткометражки _disabled
+  // if((props.isSaved && props.savedMovies.length >= 1) || (props.cards.length >=1 && !props.isSaved)) {}
+  // React.useEffect(() => {
+  //   if (isSavedMoviesPage) {
+  //     if  (props.savedMovies.length >= 1) {
+  //       console.log(props.savedMovies.length);
+  //       setIsEmptyInput(false);
+  //     }
+  //   } else { 
+  //     if (isMoviesPage) {
+  //     if (props.cards.length >= 1) {
+  //       setIsEmptyInput(false);
+  //       console.log(props.cards.length);
+  //     }}
+  //   }
+  // }, [isEmptyInput]);
+
   return (
     <section className='search'>
       <div className='search__container'>
@@ -40,7 +59,7 @@ function SearchForm(props) {
         <div className='search__short'>
             <p className='search__short_title'>Короткометражки</p>
             <div onClick={handlePick}
-            className={`search__short_button ${!isValid ? '_disabled' : ''}`}> {/*зависит от того, есть найденные фильмы или нет*/}
+            className='search__short_button'/*{`search__short_button ${isEmptyInput ? '_disabled': ''}`}*/ >
               <div className={`search__short_disk ${isShortMovie ? '_isChoosenButton' : ''}`}>
               </div>
             </div>
